6ddb9d780f5ef57a7b6b26869400fdea8553a2b8
Abhinav Sharma <abhi18av@gmail.com>
2023-05-19 16:27:48 +0200

    iteration initial setup [ci skip]

--- lib/WorkflowCholera_analysis_nf.groovy
+++ lib/WorkflowCholera_analysis_nf.groovy
@@ -11,11 +11,12 @@ class WorkflowCholera_analysis_nf {
     // Check and validate parameters
     //
     public static void initialise(params, log) {
-        
 
+/*
         if (!params.fasta) {
             Nextflow.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
         }
+*/
     }
 
     //
