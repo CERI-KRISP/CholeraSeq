174421d0fe6fedd4c7849a75e6def04c3b51d550
Abhinav Sharma <abhi18av@gmail.com>
2023-06-29 07:21:18 +0200

    tweak the cluster separator script [ci skip]

--- bin/split_clusters.bb.clj
+++ bin/split_clusters.bb.clj
@@ -10,10 +10,10 @@
                   :id (first vkt)})
        csv-data-list))
 
-
+;: TODO Accommodate a CSV flag
 (defn- read-csv-data-edn [input-file]
   (with-open [reader (io/reader (io/file input-file))]
-    (doall (->> (csv/read-csv reader )
+    (doall (->> (csv/read-csv reader #_#_:separator \tab )
                 (drop 1 )
                 csv-data->maps
                 (group-by :cluster )))))
@@ -27,21 +27,38 @@
 
 (defn- write-cluster-txt [edn-data cluster-id]
   (spit
-    (str "cluster." (name cluster-id) ".txt")
+    (str "cluster." (name cluster-id) ".csv")
     (create-cluster-str (get edn-data cluster-id))))
 
-(defn separate-clusters [cluster-file]
+
+(defn separate-clusters [{:keys [cluster-file]}]
   (let [data (read-csv-data-edn cluster-file)
         cluster-id-list (keys data)]
-    (doall (map (fn [cluster-id] (write-cluster-txt data cluster-id ))
-         cluster-id-list))))
+    (doall (map (fn [cluster-id] (write-cluster-txt data cluster-id))
+                cluster-id-list))))
+
+;; (separate-clusters "/Users/abhi/projects/ceri-choleraseq/_scratch/fastbaps_cluster/clusters.csv")
+
+(def separate-clusters-cli-opts
+  {:csv     {:desc    "Input CSV file"}})
 
 
-; (def table
-;   [{:cmds ["tsv"]   :fn separate-clusters   :args->opts [:file]} ])
+(defn help
+  [_]
+  (println
+   (str "A utility script to separate the clusters identified by FASTBAPS\n"
+        (cli/format-opts {:spec separate-clusters-cli-opts}))))
 
-; (defn -main [& args]
-;   (cli/dispatch table args {:coerce {:depth :long}}))
 
-(separate-clusters (first *command-line-args*))
+(def cli-args
+  [
+   {:cmds ["csv"]
+    :fn  #(separate-clusters (:opts %))
+    :spec separate-clusters-cli-opts
+    :args->opts [:cluster-file]}
+
+   {:cmds [] :fn help}])
+
+
+(cli/dispatch cli-args *command-line-args* {:coerce {:depth :long}})
 
